9634ff349f8f84b47ed8e5ac808820a5275f41da
Prakash Ashokan <122374176+iamprakash89@users.noreply.github.com>
2024-08-14 02:55:38 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "prakash_hotstar_demo" # Replace with your actual S3 bucket name
+    bucket = "prakashhotstardemo" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
